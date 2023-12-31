1a54bd20a68f9dc711c9ff84d1de95910256c66d
sywu22 <senyeungwu22@gmail.com>
2023-12-31 11:44:53 -0500

    assignment-4-part-1 unit tests passed

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,31 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    DEBUG_LOG("Inside threadfunc.");
+
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+
+    if (pthread_mutex_lock(thread_func_args->mutex) != 0)
+    {
+        ERROR_LOG("Failed to pthread_mutex_lock");
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+
+    if (pthread_mutex_unlock(thread_func_args->mutex) != 0)
+    {
+        ERROR_LOG("Failed to pthread_mutex_unlock");
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    thread_func_args->thread_complete_success = true;
+
     return thread_param;
 }
 
@@ -28,6 +53,25 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+    struct thread_data* thread_data = malloc(sizeof(struct thread_data));
+    if (thread_data == NULL) {
+        ERROR_LOG("Failed to malloc thread_data.");
+        return false;
+    }
+
+    thread_data->mutex = mutex;
+    thread_data->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_data->wait_to_release_ms = wait_to_release_ms;
+
+    int result = pthread_create(thread, NULL, threadfunc, (void*)thread_data);
+    if (result != 0) {
+        ERROR_LOG("Failed to pthread_create.");
+        free(thread_data);
+        return false;
+    }
+
+    return true;
+
 }
 
